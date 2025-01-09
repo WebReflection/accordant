8fdecb3a0c50590acc30c3cb8385c9d22b1cd46c
webreflection <andrea.giammarchi@gmail.com>
2025-01-09 14:12:32 +0100

    Added tests for persistent, IDB based, SQLite

--- test/shared-worker.js
+++ test/shared-worker.js
@@ -3,6 +3,22 @@ import { broadcast, exports } from '../src/shared.js';
 const SharedWorker = Math.random();
 let ports = 0;
 
+import initSQLite from 'https://cdn.jsdelivr.net/npm/@webreflection/sql.js/database.js';
+const Database = await initSQLite('accordant');
+
+const db = new Database('test.db');
+
+db.run('CREATE TABLE IF NOT EXISTS hello (a int, b char)');
+
+db.each('SELECT COUNT(*) AS fields FROM hello', null, async row => {
+  if (!row.fields) {
+    db.run(`INSERT INTO hello VALUES (0, 'hello')`);
+    db.run(`INSERT INTO hello VALUES (1, 'world')`);
+    await db.save();
+  }
+  db.each('SELECT * FROM hello', null, console.log);
+});
+
 exports({
   random: () => ({ SharedWorker }),
 });
--- test/worker.js
+++ test/worker.js
@@ -1,3 +1,4 @@
+import asTemplateStringsArray from 'https://esm.run/template-strings-array';
 import { broadcast, exports } from '../src/worker.js';
 
 exports({
@@ -6,6 +7,10 @@ exports({
 
 exports({
   random: () => ({ Worker: Math.random() }),
+  tag(template, ...values) {
+    template = asTemplateStringsArray(template);
+    console.log({ template, values });
+  }
 });
 
 broadcast('worker:connected');
